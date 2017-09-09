de09e612ba7bb90bb1cf772649c9737fc1c94b48
David Kuruvilla <dapawn@gmail.com>
2017-09-09 00:12:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget () {
+  return document.getElementById("nested").getElementsByClassName("target")[0]
+}
+
+function increaseRankBy (n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+  for (let i=0; i<rankedLists.length; i++) {
+    for (let j=0; j<rankedLists[i].children.length; j++) {
+      rankedLists[i].children[j].innerHTML = (parseInt(rankedLists[i].children[j].innerHTML) + n).toString();
+    }
+  }
+}
+
+
+function deepestChild() {
+  let current = document.getElementById("grand-node").children;
+  console.log(current.toString())
+  let next = []
+
+  while (current) {
+    if (current.length === 0) {
+      return current
+    }
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+      }
+    }
+    current = next.shift()
+    current = current.children
+  }
+  return current
+}
+
+deepestChild()
